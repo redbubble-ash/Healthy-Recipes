ea1412430afd5225a11522834023d83af8376968
Your Name <xiejing418@gmail.com>
2019-08-01 00:48:47 -0700

    deploy MongoDB app to the web, using Heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 4000;
+var PORT = process.env.PORT ||4000;
 
 // Initialize Express
 var app = express();
